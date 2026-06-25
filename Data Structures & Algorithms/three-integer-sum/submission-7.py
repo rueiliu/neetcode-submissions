4de75f076731413bb6d2df5f4ee24bdf752b2b64
rueiliu <58811813+rueiliu@users.noreply.github.com>
2026-06-25 16:39:59 +0800

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,59 @@
+'''
+1. Understand
+input: integer array nums
+output: triplets of sum = 0 in a list, non sequential
+indices are distinct
+edge cases: many duplicate values
+
+2. Plan
+
+two pointer technique
+left right, and scan thorugh the middle elements everytime
+use unumertae so can track the index
+no duplicates
+
+
+
+
+
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+        nums.sort()
+
+        #set a fixed position and apply the two pointer technique
+        #use unumertae so can track the index
+
+        for i, a in enumerate(nums):
+            if i > 0 and nums[i-1] == a:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                constant = a + nums[left] + nums[right]
+                
+                if constant > 0:
+                    right -= 1
+                elif constant < 0:
+                    left += 1
+                else:
+                    res.append([a, nums[right], nums[left]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+        return res
+     
+
+          
+
+
+
+
+        
