1999fc7be3cfa39c5dbada7ec44a0c7b80ef1b3f
rueiliu <58811813+rueiliu@users.noreply.github.com>
2026-07-02 14:11:38 +0800

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.py
@@ -0,0 +1,64 @@
+'''
+DFS
+when converting tree to string, user preorder traversla(root, left, right) 
+and recursive function, when encounter None, store N otherwise values
+
+
+#decode
+extract values from res
+create a pointer that goes through res
+when res is N add None to tree
+then use recursive function to add the nodes 
+
+
+
+
+
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        
+        def dfs(node):
+            if not node:
+                res.append("N")
+                # when encounter N, it means the tree has ended here, so move to another path
+                return 
+            res.append(str(node.val))
+            dfs.left = dfs(node.left)
+            dfs.right = dfs(node.right)
+        
+        dfs(root)
+        return ",".join(res)
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        vals = data.split(",")
+        self.i = 0
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+
+
+
+
+
