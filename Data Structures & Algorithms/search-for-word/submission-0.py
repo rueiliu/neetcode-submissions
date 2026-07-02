0329a9ea6ec7e670ce2daf68fe6bf3dfa5f90ec6
rueiliu <58811813+rueiliu@users.noreply.github.com>
2026-07-03 02:08:56 +0800

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,45 @@
+'''
+dfs!
+
+
+
+'''
+
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROW, COL = len(board), len(board[0])
+        path = set() # store elements that have been used
+
+
+
+        def dfs(r, c, i): # i is the count of approve elements
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= ROW or c >= COL or i > len(word) 
+            or word[i] != board[r][c] or (r, c) in path):
+                return False
+
+            path.add((r,c))
+
+            res = (dfs(r+1, c, i+1) or
+                  dfs(r-1, c, i+1) or
+                  dfs(r, c+1, i+1) or
+                  dfs(r, c-1, i+1) )
+
+            path.remove((r, c))
+
+            return res
+
+        
+        for r in range(ROW):
+            for c in range(COL):
+                if dfs(r,c,0):
+                    return True
+
+        return False
+
+
+
+        
