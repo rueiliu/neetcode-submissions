892426c4c488b0686fbe110535bb02e4208605fe
rueiliu <58811813+rueiliu@users.noreply.github.com>
2026-07-11 23:58:34 +0800

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.py
@@ -0,0 +1,28 @@
+'''
+create res for maximum result(set default as maximum value, like singel)
+create curMax, curMin(min for if its super negative and next one is negative then it will become super postivie)
+
+if num == 0, reset the calculation
+
+use for loop to update curMax, curmin
+then compare with res
+
+return res
+
+
+
+'''
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curMax, curMin = 1, 1
+
+        for num in nums:
+            if num == 0:
+                curMax, curMin = 1,1
+                continue
+            tmp = curMax
+            curMax = max(num, num*curMax, num*curMin)
+            curMin = min(num, tmp*num, num*curMin)
+            res = max(res, curMax)
+        return res
