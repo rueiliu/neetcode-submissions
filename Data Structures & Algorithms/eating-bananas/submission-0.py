f8d4d91d17f4c3e990b697ef25b96f6366f76a0f
rueiliu <58811813+rueiliu@users.noreply.github.com>
2026-07-23 13:27:31 +0800

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,35 @@
+'''
+binary search - search from 1 to max(piles) and return minimum hour
+
+
+
+'''
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        l, r = 1, max(piles) #anything above max(piles) doesn't matter
+
+
+        while l <= r:
+            k = (l + r) // 2
+            totaltime = 0
+
+            for pile in piles:
+                totaltime += math.ceil(pile/k)
+
+            if totaltime <= h:
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+
+
+        return res
+
+
+      
+
+
+        
